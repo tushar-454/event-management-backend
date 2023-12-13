d74cc16d7c631d3a372a5eb88a8876bf1da4d25c
tushar-454 <imtushar454@gmail.com>
2023-12-13 17:57:30 +0600

    delete api added

--- index.js
+++ index.js
@@ -45,10 +45,26 @@ async function run() {
       res.send(result);
     });
 
+    // get booking cart
+    app.get('/cart/:email', async (req, res) => {
+      const { email } = req.params;
+      const result = await bookingCollection.find({ email: email }).toArray();
+      res.send(result);
+    });
+
     // add cart item in database
     app.post('/booking', async (req, res) => {
       const bookingData = req.body;
       const result = await bookingCollection.insertOne(bookingData);
+      res.send(result);
+    });
+
+    // delete cart from database
+    app.delete('/cart/:id', async (req, res) => {
+      const id = req.params.id;
+      const result = await bookingCollection.deleteOne({
+        _id: new ObjectId(id),
+      });
       res.send(result);
     });
 
