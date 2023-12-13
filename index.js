bcc31d45fc1d2f977be81b7562f7997638d21f87
tushar-454 <imtushar454@gmail.com>
2023-12-13 18:26:41 +0600

    server deploy in vercel and origin set

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 app.use(express.json());
 app.use(
   cors({
-    origin: ['http://localhost:5173'],
+    origin: ['https://event-management-4dbcc.web.app'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   })
