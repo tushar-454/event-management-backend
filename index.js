42aa943aa6b158e8457d48fee1c13957b01894f9
tushar-454 <imtushar454@gmail.com>
2023-12-13 16:55:47 +0600

    server init and make some api for service get and add to cart service

---
+++ index.js
@@ -0,0 +1,71 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT || 4000;
+const cors = require('cors');
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  })
+);
+
+const uri = process.env.URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const eventManagementDB = client.db('eventManagementDB');
+    const servicesCollection = eventManagementDB.collection('services');
+    const bookingCollection = eventManagementDB.collection('booking');
+
+    // api database data
+    app.get('/services', async (req, res) => {
+      const result = await servicesCollection.find().toArray();
+      res.send(result);
+    });
+
+    // get one services using id
+    app.get('/services/:id', async (req, res) => {
+      const id = req.params.id;
+      const result = await servicesCollection.findOne({
+        _id: new ObjectId(id),
+      });
+      res.send(result);
+    });
+
+    // add cart item in database
+    app.post('/booking', async (req, res) => {
+      const bookingData = req.body;
+      const result = await bookingCollection.insertOne(bookingData);
+      res.send(result);
+    });
+
+    await client.db('admin').command({ ping: 1 });
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('<h1>api is working</h1>');
+});
+
+app.listen(port, () => {
+  console.log(`server is running http://localhost:${port}`);
+});
